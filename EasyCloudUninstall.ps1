d637f7ba2d66c5c3c26a15cfdec00ad1abe669d3
Goldenlagen <mathieu.ranieri@outlook.fr>
2022-06-11 21:48:50 +0200

    Adding uninstaller & VMDeployment final version & Install final version

--- EasyCloudUninstall.ps1
+++ EasyCloudUninstall.ps1
@@ -1,27 +1,25 @@
 ﻿Function Uninstall-EasyCloud {
     Process {
         Try {
-            $Folder = $PSScriptRoot
-            
+            $Folder = (Get-Location).Path
             
             $moduleFolder = ";$Folder\App\Modules"
             $moduleFolder = $moduleFolder.replace(" ","")
 
-            Write-Host $moduleFolder
-            Read-Host "Waiting"      
-        
-            Remove-SmbShare -Name Iso
-
-        
             $str = $env:PSModulePath
+            $str.Contains($moduleFolder)
 
+            Remove-SmbShare -Name Iso
+            
             If($str.Contains($moduleFolder)) {
                 $str = $str.replace($moduleFolder, $null)
 
                 [Environment]::SetEnvironmentVariable("PSModulePath", $str, "Machine")
             }
 
-            Remove-Item -Path $Folder
+            Remove-Item -Path "$Folder\App"
+            Remove-Item -Path "$Folder\Configuration"
+
         } Catch {
             Write-Error "Error"
         }
