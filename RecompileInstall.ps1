11505bf8b9c9673e8585371c0a288666218525a2
Goldenlagen <mathieu.ranieri@outlook.fr>
2022-06-24 23:39:16 +0200

    Update repo

--- RecompileInstall.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location "C:\EasyCloud_PSModules_GIT"
-ps2exe -inputFile "EasyCloudInstall.ps1" -outputFile "InstallationPackage\EasyCloudInstaller.exe" -iconFile "EasyCloudLogo.ico"
--- RecompileUninstall.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location "C:\EasyCloud_PSModules_GIT"
-ps2exe -inputFile "EasyCloudUninstall.ps1" -outputFile "InstallationPackage\EasyCloudUninstaller.exe" -iconFile "EasyCloudLogo.ico"
